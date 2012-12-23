e66dcd6371a1ba701b3e2486bdc311e9730a0ef5
Jonas Markussen <jonassm@ifi.uio.no>
2012-12-23 23:54:35 +0100

    Problem 14

---
+++ 14/collatz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+uint64_t f(uint64_t n)
+{
+	uint64_t step = 0;
+	while (n != 1) {
+		++step;
+
+		if (n % 2 == 0)
+			n /= 2;
+		else
+			n = 3*n + 1;	
+	}
+
+	return step+1;
+//	if (n == 1) {
+//		return 1;
+//	} else if (n % 2 == 0) {
+//		return 1 + f(n/2);
+//	} else {
+//		return 1 + f(3*n + 1);
+//	}
+}
+
+
+int main(void)
+{
+	uint64_t i, n, m, k;
+  	for (i = 2, n = 0; i < 1000000; ++i) {
+  		m = f(i);
+  		if (m > n) {
+  			n = m;
+  			k = i;
+			printf("%llu:%llu\n", k, n);
+  		}
+  	}
+  	printf("%llu\n", k);
+	return 0;
+}
