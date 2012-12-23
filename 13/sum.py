ee684593552e3d7b6f7ba26f84a1c56d8ff6c967
Jonas Markussen <jonassm@ifi.uio.no>
2012-12-23 23:49:24 +0100

    simplified

--- 13/sum.py
+++ 13/sum.py
@@ -3,5 +3,4 @@ import urllib2 as url
 import re
 
 numbers = [ int(i) for i in re.findall("([0-9]{50})", url.urlopen("http://projecteuler.net/problem=13").read()) ]
-s = "%d" % sum(numbers)
-print s[:10]
+print str(sum(numbers))[:10]
