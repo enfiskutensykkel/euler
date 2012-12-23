49c14401e888c77b92f33618b1e1b5cdae432ac2
Jonas Markussen <jonassm@ifi.uio.no>
2012-12-23 23:42:04 +0100

    Safety

--- descs.py
+++ descs.py
@@ -5,9 +5,15 @@
 
 import urllib2 as url, os, re
 
-dirs = [ name for name in os.listdir(".") if os.path.isdir(os.path.join(".", name)) and not name == '.git' ]
+dirs = [ name for name in os.listdir(".") if os.path.isdir(os.path.join(".", name)) and name.isdigit() ]
 
+override = False
 for d in dirs:
+	if not override and os.path.isfile(os.path.join(os.path.join(".",d), 'README.md')):
+		continue
+
+	print "Retrieving problem description #%s" % d
+
 	r = url.urlopen("http://projecteuler.net/problem=%s" % d).read()
 	title = re.findall(r'<h2>(.*?)</h2>', r)[0].replace("\r", "")
 	content = re.findall(r'<div\s+class="problem_content".*?>(.*)</div><br />\s*<br /></div>', r, re.DOTALL | re.MULTILINE)[0].replace("\r", "")
@@ -19,4 +25,5 @@ for d in dirs:
 
 	link = "\n\n[Go to the problem description](http://projecteuler.net/problem=%s)\n" % d
 
+	print "Writing description #%s to file" % d
 	open(os.path.join(os.path.join(".",d), 'README.md'), 'w').write(title + line + content + link)
