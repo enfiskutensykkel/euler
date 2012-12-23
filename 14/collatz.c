9f027c1daff8b0b1184c9efd5a0b76a63405564c
Jonas Markussen <jonassm@ifi.uio.no>
2012-12-24 00:06:10 +0100

    Minor refactoring

--- 14/collatz.c
+++ 14/collatz.c
@@ -4,6 +4,7 @@
 uint64_t f(uint64_t n)
 {
 	uint64_t step = 0;
+
 	while (n != 1) {
 		++step;
 
@@ -13,28 +14,38 @@ uint64_t f(uint64_t n)
 			n = 3*n + 1;	
 	}
 
-	return step+1;
-//	if (n == 1) {
-//		return 1;
-//	} else if (n % 2 == 0) {
-//		return 1 + f(n/2);
-//	} else {
-//		return 1 + f(3*n + 1);
-//	}
+	return step + 1;
+}
+
+
+uint64_t f_rec(uint64_t n)
+{
+	/* Turn on -foptimize-sibling-calls for tail-call elimination 
+	 * (Not that it really matters in this case [recursion isn't too deep])
+	 */
+	if (n == 1)
+		return 1;
+	else if (n % 2 == 0)
+		return 1 + f(n/2);
+	else 
+		return 1 + f(3*n + 1);
 }
 
 
 int main(void)
 {
 	uint64_t i, n, m, k;
+
   	for (i = 2, n = 0; i < 1000000; ++i) {
-  		m = f(i);
+  		
+		m = f_rec(i);
+
   		if (m > n) {
   			n = m;
   			k = i;
 			printf("%llu:%llu\n", k, n);
   		}
   	}
-  	printf("%llu\n", k);
+
 	return 0;
 }
