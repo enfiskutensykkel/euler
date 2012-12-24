aa1b542842e210ab72a23afc990ab3ea5aea8cd6
Jonas Markussen <jonassm@ifi.uio.no>
2012-12-24 02:55:52 +0100

    minor

--- 51-75/67/merge.py
+++ 51-75/67/merge.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
 	import re, urllib2 as url
 
 	triangle = []
+	#for line in open("triangle.txt").readlines():
 	for line in url.urlopen("http://projecteuler.net/project/triangle.txt").readlines():
 		row = re.findall("([0-9]+)", line)
 		triangle.append([ int(x) for x in row ])
