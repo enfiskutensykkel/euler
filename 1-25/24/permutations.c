32a05d62adac079a67ec88857dde768e3a25567a
Jonas Markussen <jonassm@ifi.uio.no>
2012-12-25 00:58:08 +0100

    lexicographic permutation

---
+++ 1-25/24/permutations.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+int64_t factorial(int64_t n, int64_t a)
+{
+	if (n <= 1)
+		return a;
+
+	/* tail call (so that the compiler can do some magic) */
+	return factorial(n-1, a*n);
+}
+
+
+int main(void)
+{
+	int tbl[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int64_t num, idx, fac, lim, i, j;
+
+	num = 0; lim = 1000000L - 1; /* target is the millionth permutation */
+	for (i = 1; lim > 0 && i < 10; ++i) {
+		fac = factorial(10 - i, 1);
+
+		idx = lim / fac;
+		lim = lim % fac;
+
+		num = 10 * num + tbl[idx];
+		for (j = idx; j < 9; ++j)
+			tbl[j] = tbl[j+1];
+		tbl[j] = -1;
+	}
+	
+	for (i = 0; tbl[i] != -1 && i < 10; ++i)
+		num = 10 * num + tbl[i];
+
+	printf("%lld\n", num);
+	return 0;
+}
