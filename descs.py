b7d1befe14afef884ad8d616cb2780e3420ac7ae
Jonas Markussen <jonassm@ifi.uio.no>
2012-12-24 02:17:59 +0100

    reorganized

--- descs.py
+++ descs.py
@@ -1,29 +1,49 @@
 #!/usr/bin/env python
 
-# This script retrieves all the problem descriptions, and formats them to
-# mark down. Run this in the root directory.
+# This script retrieves problem descriptions from the project Euler website
+# and stores them as README files with Markdown syntex.
+#
+# It assumes that solved problems are organized in directories corresponding
+# to their ID number. For example, the solution to problem 18 should be stored
+# as:
+#    ./1-25/18/whatever
+#
+# This script will then recognize 18 and download the problem description and
+# put it in ./1-25/18
+
+### parameters ###
+override = True
+root = "."
+name = "README.md"
+### end parameters ###
+
 
 import urllib2 as url, os, re
 
-dirs = [ name for name in os.listdir(".") if os.path.isdir(os.path.join(".", name)) and name.isdigit() ]
+for subdir in map(lambda name: os.path.join(root, name), filter(lambda name: re.match(r'[0-9]+\-[0-9]+', name) != None, [ name for name in os.listdir(root) if os.path.isdir(os.path.join(root, name)) ])):
+	for directory, problem in [ (os.path.join(subdir, d), d) for d in os.listdir(subdir) if os.path.isdir(os.path.join(subdir, d)) and d.isdigit() ]:
 
-override = False
-for d in dirs:
-	if not override and os.path.isfile(os.path.join(os.path.join(".",d), 'README.md')):
-		continue
+		# Don't overwrite existing file
+		if not override and os.path.isfile(os.path.join(directory, name)):
+			continue
 
-	print "Retrieving problem description #%s" % d
 
-	r = url.urlopen("http://projecteuler.net/problem=%s" % d).read()
-	title = re.findall(r'<h2>(.*?)</h2>', r)[0].replace("\r", "")
-	content = re.findall(r'<div\s+class="problem_content".*?>(.*)</div><br />\s*<br /></div>', r, re.DOTALL | re.MULTILINE)[0].replace("\r", "")
-	#content = url.urlopen("http://projecteuler.net/minimal=%s" % d).read().replace("\r","")
+		# Retrieve problem description
+		print "#%s downloading..." % problem
+		result = url.urlopen("http://projecteuler.net/problem=%s" % problem).read()
 
-	if not title[-1] == "\n":
-		title += "\n"
-	line = "".join(['='] * (len(title)-1)) + '\n'
 
-	link = "\n\n[Go to the problem description](http://projecteuler.net/problem=%s)\n" % d
+		# Process data
+		title = re.findall(r'<h2>(.*?)</h2>', result)[0].replace("\r", "")
+		contents = re.findall(r'<div\s+class="problem_content".*?>(.*)</div><br />\s*<br /></div>', result, re.DOTALL | re.MULTILINE)[0].replace("\r", "")
+		#contents = url.urlopen("http://projecteuler.net/minimal=%s" % d).read().replace("\r","")
+		if not title[-1] == "\n":
+			title += "\n"
+		line = "".join(['='] * (len(title)-1)) + '\n'
+		link = "\n\n[Go to the problem description](http://projecteuler.net/problem=%s)\n" % problem
 
-	print "Writing description #%s to file" % d
-	open(os.path.join(os.path.join(".",d), 'README.md'), 'w').write(title + line + content + link)
+
+		# Write to file
+		print "#%s writing..." % s
+		open(os.path.join(directory, name), 'w').write(title + line + content + link)
+
