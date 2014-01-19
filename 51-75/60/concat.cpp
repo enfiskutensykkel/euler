a8dfe92c814a3da8dc80695beb91ae6b8f635fe8
Jonas Markussen <jonassm@ifi.uio.no>
2014-01-19 16:33:34 +0100

    did a pseudo-optimisation, runs way slower but is more generic than before

--- 51-75/60/concat.cpp
+++ 51-75/60/concat.cpp
@@ -3,17 +3,37 @@
 #include <cmath>
 
 #define TARGET 5
-#define NUMBERS 100000000
+#define LIMIT 30000
 
+std::vector<unsigned> primes;
 std::vector<bool> numbers;
 
+
+
+inline bool is_prime(unsigned x)
+{
+	if (x < numbers.size())
+	{
+		return numbers[x];
+	}
+
+	for (unsigned i = 2; i <= (unsigned) sqrt(x) + 1; ++i)
+	{
+		if (x % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
 
 
 
 inline void print_set(const std::vector<unsigned>& set)
 {
 	unsigned sum = 0;
-	for (auto i = set.begin(); i != set.end(); ++i)
+	for (std::vector<unsigned>::const_iterator i = set.begin(); i != set.end(); ++i)
 	{
 		printf("%u  ", *i);
 		sum += *i;
@@ -41,11 +61,15 @@ void sieve(unsigned N)
 
 	numbers[0] = false;
 	numbers[1] = false;
-	for (unsigned i = 2; i < (unsigned) sqrt(N) + 1; ++i)
+	for (unsigned i = 2; i < N; ++i)
 	{
-		for (unsigned j = i << 1; numbers[i] && j < N; j += i)
+		if (numbers[i])
 		{
-			numbers[j] = false;
+			primes.push_back(i);
+			for (unsigned j = i << 1; j < N; j += i)
+			{
+				numbers[j] = false;
+			}
 		}
 	}
 }
@@ -54,13 +78,9 @@ void sieve(unsigned N)
 
 inline bool verify(const std::vector<unsigned>& set, unsigned x)
 {
-	const unsigned lim = numbers.size();
-
-	for (auto i = set.begin(); i != set.end(); ++i)
+	for (std::vector<unsigned>::const_iterator i = set.begin(); i != set.end(); ++i)
 	{
-		unsigned v = concat(*i, x);
-		unsigned w = concat(x, *i);
-		if (v >= lim || w >= lim || !numbers[v] || !numbers[w])
+		if (!is_prime(concat(*i, x)) || !is_prime(concat(x, *i)))
 		{
 			return false;
 		}
@@ -71,25 +91,29 @@ inline bool verify(const std::vector<unsigned>& set, unsigned x)
 
 
 
-inline void test(std::vector<unsigned>& set)
+inline void test(std::vector<unsigned>& set, std::vector<unsigned>::const_iterator p)
 {
-	const unsigned lim = numbers.size();
-
-	for (unsigned i = set.back() + 1; i < (unsigned) sqrt(lim) + 1; ++i)
+	
+	while (p != primes.end())
 	{
-		if (numbers[i] && verify(set, i))
+		if (verify(set, *p))
 		{
-			set.push_back(i);
+			set.push_back(*p);
+
 			if (set.size() == TARGET)
 			{
 				print_set(set);
 			}
 			else
 			{
-				test(set);
+				std::vector<unsigned>::const_iterator i = p;
+				test(set, ++i);
 			}
+			
 			set.pop_back();
 		}
+
+		++p;
 	}
 }
 
@@ -99,19 +123,17 @@ inline void test(std::vector<unsigned>& set)
 int main()
 {
 	puts("Calculating primes...");
-	sieve(NUMBERS);
+	sieve(LIMIT);
 
 	puts("Finding prime pair sets...");
 	std::vector<unsigned> set;
 
-	for (unsigned i = 3; i < (unsigned) sqrt(numbers.size()) + 1; ++i)
+	for (std::vector<unsigned>::const_iterator p = primes.begin(); p != primes.end(); ++p)
 	{
-		if (numbers[i])
-		{
-			set.clear();
-			set.push_back(i);
-			test(set);
-		}
+		set.clear();
+		set.push_back(*p);
+		std::vector<unsigned>::const_iterator i = p;
+		test(set, ++i);
 	}
 
 	puts("Done");
