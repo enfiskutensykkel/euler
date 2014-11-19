fce5efa8a4a2035b358d503a95b44cc934959ee4
Jonas Markussen <jonassm@ifi.uio.no>
2014-11-19 15:15:52 +0100

    Update triplet.c

--- 1-25/9/triplet.c
+++ 1-25/9/triplet.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-#define N 1000.0
-
 /* Not efficient, but we can bruteforce this 
  * You need to multiply the three factors manually, 
  * since it overflows with printf.
  */
 int main(void)
 {
-	double a, b, c;
+	double a, b;
 
-	for (a = 0.0; a < N; ++a)
-		for (b = a+1.0; b < N; ++b)
+	for (a = 0; a < 1000; ++a)
+		for (b = a + 1; b < 1000; ++b)
 			if (a + b + sqrt(pow(a,2) + pow(b,2)) == 1000) 
 				printf("%g %g %g\n", a, b, 1000-a-b);
 
